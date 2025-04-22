715ffc41769f67a324cd632b2f0008e433aba71a
Kew-Sanphit <66111384@dpu.ac.th>
2025-04-23 01:39:24 +0700

    เพิ่ม สัตว์

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import authRoutes from './routes/auth-routes';  // ใช้ express.Application จากไฟล์ auth-routes.ts
+import petsRoutes from "./routes/pets";
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());  // ใช้ body-parser ใน express
 
 app.use(authRoutes);  // ใช้ app ที่ส่งออกมาจาก auth-routes.ts
+app.use("/pets", petsRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
