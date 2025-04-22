ef92f081181d44b71d062c55b202fcd734bdf74c
NicasioDx <maekinza5@gmail.com>
2025-04-22 21:26:27 +0700

    api_login
    แก้apiของloginพร้อมเมท่อสนเรืเสร็จเช้าหน้าhomeแทนจองคิว

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,12 +4,14 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import authRoutes from './routes/auth-routes';  // ใช้ express.Application จากไฟล์ auth-routes.ts
 
+
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());  // ใช้ body-parser ใน express
 
-app.use(authRoutes);  // ใช้ app ที่ส่งออกมาจาก auth-routes.ts
+app.use('/api/auth', authRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
